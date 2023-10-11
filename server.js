4d1fb867d8e079019f58631e06751a1d224348b9
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-10-11 17:22:26 +0900

    feat:dotenv & project configuration

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const http = require("http");
 
 const mongodb = require("mongodb");
 
-connectionString =
-  "mongodb+srv://vakhobovabdurauf:xw8uK63Wau1znpmG@ben.kgwux9o.mongodb.net/Reja?retryWrites=true&w=majority";
+connectionString = process.env.MONGO_URL;
 
 mongodb.connect(
   connectionString,
@@ -19,7 +21,7 @@ mongodb.connect(
       const app = require("./app");
 
       const server = http.createServer(app);
-      let PORT = 3001;
+      let PORT = process.env.PORT || 3001;
       server.listen(PORT, function () {
         console.log(
           `The server is running successfully on port: ${PORT}, http://localhost:${PORT}  `
