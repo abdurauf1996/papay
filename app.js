9c58b2256877386acc7d119732b3484ea51f5e46
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-10-13 17:07:10 +0900

    feat: member controllerlarni qurish

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("Web Serverni  boshlash");
 const express = require("express");
 const app = express();
-
+const router = require("./router");
 //MongoDB  choqrish
 const db = require("./server").db();
 const mongodb = require("mongodb");
@@ -19,5 +19,7 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
+
+app.use("/", router);
 
 module.exports = app;
---
+++ router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const memberController = require("./contollers/memberController");
+
+//memberga dahildor routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
+//boshqa routerlar
+router.get("/menu", (req, res) => {
+  res.send("Menu sahifadasiz");
+});
+
+router.get("/community", (req, res) => {
+  res.send("Jamiyat sahifadasiz");
+});
+
+module.exports = router;
