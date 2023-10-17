216cff2a13d722cc399e02d2eee893de6828af21
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-10-17 11:39:11 +0900

    feat: bcrypt package asosoda passwordni hash qilish

--- models/Meber.js
+++ models/Meber.js
@@ -1,6 +1,7 @@
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
+const bcrypt = require("bcryptjs");
 
 class Member {
   constructor() {
@@ -8,6 +9,8 @@ class Member {
   }
   async signupData(input) {
     try {
+      const salt = await bcrypt.genSalt();
+      input.mb_password = await bcrypt.hash(input.mb_password, salt);
       const new_member = new this.memberModel(input);
       let result;
 
@@ -31,8 +34,12 @@ class Member {
         .findOne({ mb_nick: input.mb_nick }, { mb_nick: 1, mb_password: 1 })
         .exec();
       assert.ok(member, Definer.auth_err3);
-      const isMatch = input.mb_password === member.mb_password;
-      assert.ok(member, Definer.auth_err4);
+
+      const isMatch = await bcrypt.compare(
+        input.mb_password,
+        member.mb_password
+      );
+      assert.ok(isMatch, Definer.auth_err4);
       return await this.memberModel.findOne({ mb_nick: input.mb_nick }).exec();
     } catch (err) {
       throw err;
