6313ac1c385104609057025950df9856ecee5e35
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-11-07 16:33:31 +0900

    fix: modify sign-up link on adminka

--- contollers/restaurantController.js
+++ contollers/restaurantController.js
@@ -125,3 +125,27 @@ restaurantController.checkSessions = (req, res) => {
     res.json({ state: "fail", message: "You are not authenticated" });
   }
 };
+
+restaurantController.validateAdmin = (req, res, next) => {
+  if (req.session?.member?.mb_type === "ADMIN") {
+    req.member = req.session.member;
+    next();
+  } else {
+    const html = `<script>
+              alert('Admin page:Permission denied!');
+              window.location.replace('/resto');
+                  </script>`;
+    res.end(html);
+  }
+};
+
+restaurantController.getAllRestaurants = (req, res) => {
+  try {
+    console.log("Get cont/getAllRestaurants");
+    // toda: hama restaurantlarni dbdan chaqiramiz
+    res.render("all-restaurants");
+  } catch (err) {
+    console.log(`ERROR, cont/getAllRestaurants, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
