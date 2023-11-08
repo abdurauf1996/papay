f81c7927b61afc06c6721949c55994e4b5a5961d
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-11-09 01:07:59 +0900

    fix: modify to render real resataurant data on restaurant ejs

--- contollers/restaurantController.js
+++ contollers/restaurantController.js
@@ -3,6 +3,7 @@ const Definer = require("../lib/mistake");
 const Member = require("../models/Meber");
 const Product = require("../models/Product");
 const assert = require("assert");
+const Restaurant = require("../models/Restaurant");
 let restaurantController = module.exports;
 
 restaurantController.home = (req, res) => {
@@ -139,11 +140,16 @@ restaurantController.validateAdmin = (req, res, next) => {
   }
 };
 
-restaurantController.getAllRestaurants = (req, res) => {
+restaurantController.getAllRestaurants = async (req, res) => {
   try {
     console.log("Get cont/getAllRestaurants");
-    // toda: hama restaurantlarni dbdan chaqiramiz
-    res.render("all-restaurants");
+
+    const restaurant = new Restaurant();
+    const restaurants_data = await restaurant.getAllRestaurantsData();
+    console.log(restaurants_data);
+
+    // todo: hama restaurantlarni dbdan chaqiramiz
+    res.render("all-restaurants", { restaurants_data: restaurants_data });
   } catch (err) {
     console.log(`ERROR, cont/getAllRestaurants, ${err.message}`);
     res.json({ state: "fail", message: err.message });
