f02486fdae5b0f2efb81943e563f9114ad4b604c
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-11-09 14:47:06 +0900

    fix: complete development of allrestaurants  business logic

--- contollers/restaurantController.js
+++ contollers/restaurantController.js
@@ -143,15 +143,25 @@ restaurantController.validateAdmin = (req, res, next) => {
 restaurantController.getAllRestaurants = async (req, res) => {
   try {
     console.log("Get cont/getAllRestaurants");
-
     const restaurant = new Restaurant();
     const restaurants_data = await restaurant.getAllRestaurantsData();
-    console.log(restaurants_data);
 
-    // todo: hama restaurantlarni dbdan chaqiramiz
     res.render("all-restaurants", { restaurants_data: restaurants_data });
   } catch (err) {
     console.log(`ERROR, cont/getAllRestaurants, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
+
+restaurantController.updateRestaurantByAdmin = async (req, res) => {
+  try {
+    console.log("Get cont/updateRestaurantByAdmin");
+
+    const restaurant = new Restaurant();
+    const result = await restaurant.updateRestaurantByAdminData(req.body);
+    await res.json({ state: "success", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/updateRestaurantByAdmin, ${err.message}`);
     res.json({ state: "fail", message: err.message });
   }
 };
