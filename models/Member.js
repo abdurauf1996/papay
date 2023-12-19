26210de1a1373c071f352cdb5f78a1d48c1b06e8
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-12-19 15:43:14 +0900

    fix: modify getChosenMember api by getting follow back data

--- models/Member.js
+++ models/Member.js
@@ -2,8 +2,13 @@ const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
 const bcrypt = require("bcryptjs");
-const { shapeIntoMongooseObjectId } = require("../lib/config");
+const {
+  shapeIntoMongooseObjectId,
+  lookup_auth_member_following,
+} = require("../lib/config");
 const View = require("./View");
+const { aggregate } = require("../schema/follow.model");
+const { lookup } = require("dns");
 
 class Member {
   constructor() {
@@ -49,20 +54,25 @@ class Member {
   }
   async getChosenMemberData(member, id) {
     try {
+      const auth_mb_id = shapeIntoMongooseObjectId(member?._id);
       id = shapeIntoMongooseObjectId(id);
       console.log("member:::", member);
 
+      let aggregateQuery = [
+        { $match: { _id: id, mb_status: "ACTIVE" } },
+        { $unset: "mb_password" },
+      ];
+
       if (member) {
         await this.viewChosenItemByMember(member, id, "member");
+        // todo: check auth member liked the chosen target
+        aggregateQuery.push(
+          lookup_auth_member_following(auth_mb_id, "members")
+        );
       }
 
-      const result = await this.memberModel
-        .aggregate([
-          { $match: { _id: id, mb_status: "ACTIVE" } },
-          { $unset: "mb_password" },
-          //todo: check auth member liked the chosen member
-        ])
-        .exec();
+      const result = await this.memberModel.aggregate(aggregateQuery).exec();
+
       assert.ok(result, Definer.general_err2);
       return result[0];
     } catch (err) {
